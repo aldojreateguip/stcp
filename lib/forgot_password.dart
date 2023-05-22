9330fd754ab3e4d4509f392c39a8d0941f76f504
Alisso <alisso@gmail.com>
2023-05-22 08:34:03 -0500

    logo height en %, forgot view

--- lib/forgot_password.dart
+++ lib/forgot_password.dart
@@ -4,31 +4,79 @@ import 'package:stcp/appbar.dart';
 class ForgotPassword extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return GestureDetector(
+      onTap: () {
+        // Quitar el foco del campo de texto cuando se haga clic fuera del mismo
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
         appBar: const Header(title: 'OLVIDÉ MI CONTRASEÑA', background: '#FF2D3E50', height: 56),
         body: FractionallySizedBox(
           heightFactor: 1,
           widthFactor: 1,
-          child: Container(
+          child: Stack(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 50, left: 20, right: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Por favor, ingrese el correo electrónico utilizado en su cuenta y le enviaremos un enlace para restablecer su contraseña.',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.blueGrey,
+                      ),
+                      textAlign: TextAlign.justify,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 50),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Correo electrónico',
+                          contentPadding: EdgeInsets.zero,
+                        ),
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                        cursorHeight: 25,
+                        cursorRadius: Radius.circular(2),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: FractionallySizedBox(
+                  widthFactor: 1,
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Acción a realizar cuando se presione el botón
+                      },
+                      child: Text(
+                        'ENVIAR',
+                        style: TextStyle(fontSize: 10),
+                        textScaleFactor: 2,
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(vertical: 12),
+                        primary: Color(0xFF2D3E50),
+                        onPrimary: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
+      ),
     );
   }
 }
-
-// class SecondScreen extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     final String message = ModalRoute.of(context)!.settings.arguments as String;
-
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Second Screen'),
-//       ),
-//       body: Center(
-//         child: Text(message),
-//       ),
-//     );
-//   }
-// }
-
--- lib/widgets/Login/body.dart
+++ lib/widgets/Login/body.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:stcp/forgot_password.dart';
 
 class LoginBody extends StatelessWidget {
   @override
@@ -13,126 +12,143 @@ class LoginBody extends StatelessWidget {
     const String textLogin = 'INICIAR SESIÓN';
 
     return FractionallySizedBox(
-        widthFactor: bodyWidth,
-        child: SizedBox(
-          height: bodyHeight,
-          child: Align(
-            alignment: Alignment.topCenter,
-            child: Container(
-              margin: const EdgeInsets.only(top: textMarginTop),
-              child: Column(
-                children: [
-                  Text(
-                    textLogin,
-                    textScaleFactor: textScaleFactor,
+      widthFactor: bodyWidth,
+      child: SizedBox(
+        height: bodyHeight,
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            margin: const EdgeInsets.only(top: textMarginTop),
+            child: Column(
+              children: [
+                Text(
+                  textLogin,
+                  textScaleFactor: textScaleFactor,
+                  style: TextStyle(
+                    fontSize: textFontSize,
+                    fontWeight: fontType,
+                    color: Colors.blueGrey,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Correo electrónico',
+                      contentPadding: EdgeInsets.zero,
+                    ),
                     style: TextStyle(
-                      fontSize: textFontSize,
-                      fontWeight: fontType,
-                      color: Colors.blueGrey,
+                      fontSize: 18,
                     ),
+                    cursorHeight: 25,
+                    cursorRadius: Radius.circular(2),
                   ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Correo electrónico',
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Contraseña',
+                      contentPadding: EdgeInsets.zero,
+                    ),
+                    obscureText: true,
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                    cursorHeight: 25,
+                    cursorRadius: Radius.circular(2),
+                  ),
+                ),
+                Spacer(),
+                Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.only(top: 20),
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Acción a realizar cuando se presione el botón
+                    },
+                    child: Text(
+                      'INGRESAR',
+                      style: TextStyle(fontSize: 10),
+                      textScaleFactor: 2,
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(vertical: 12),
+                      primary: Color(0xFF2D3E50),
+                      onPrimary: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
                       ),
                     ),
                   ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 35),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Contraseña',
-                      ),
-                      obscureText: true,
-                    ),
-                  ),
-                  Container(
-                    width: double.infinity,
-                    margin: EdgeInsets.only(top: 20),
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        // Acción a realizar cuando se presione el botón
-                      },
-                      child: Text(
-                        'INGRESAR',
-                        style: TextStyle(fontSize: 10),
-                        textScaleFactor: 2,
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        padding: EdgeInsets.symmetric(vertical: 12),
-                        primary: Color(0xFF2D3E50),
-                        onPrimary: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(25),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 10),
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Expanded(
-                          child: Padding(
-                            padding: EdgeInsets.only(right: 8),
-                            child: ElevatedButton(
-                              onPressed: () {
-                                // Acción a realizar cuando se presione el botón
-                              },
-                              child: Text(
-                                'REGÍSTRATE',
-                                style: TextStyle(fontSize: 13),
-                              ),
-                              style: ElevatedButton.styleFrom(
-                                padding: EdgeInsets.symmetric(vertical: 16),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(25),
-                                  side: BorderSide(
-                                      width: 3, color: Colors.blueGrey),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.only(right: 8),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Acción a realizar cuando se presione el botón
+                            },
+                            child: Text(
+                              'REGÍSTRATE',
+                              style: TextStyle(fontSize: 13),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              padding: EdgeInsets.symmetric(vertical: 16),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(25),
+                                side: BorderSide(
+                                  width: 3,
+                                  color: Colors.blueGrey,
                                 ),
-                                primary: Colors.white,
-                                onPrimary: Colors.blueGrey,
                               ),
+                              primary: Colors.white,
+                              onPrimary: Colors.blueGrey,
                             ),
                           ),
                         ),
-                        Expanded(
-                          child: Padding(
-                            padding: EdgeInsets.only(left: 8),
-                            child: ElevatedButton(
-                              onPressed: () {
-                                Navigator.pushNamed(context, '/forgot');
-                              },
-                              child: Text(
-                                'FORGOT PASSWORD',
-                                style: TextStyle(fontSize: 13),
-                              ),
-                              style: ElevatedButton.styleFrom(
-                                padding: EdgeInsets.symmetric(vertical: 16),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(25),
-                                  side: BorderSide(
-                                      width: 3, color: Colors.blueGrey),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 8),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/forgot');
+                            },
+                            child: Text(
+                              'FORGOT PASSWORD',
+                              style: TextStyle(fontSize: 13),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              padding: EdgeInsets.symmetric(vertical: 16),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(25),
+                                side: BorderSide(
+                                  width: 3,
+                                  color: Colors.blueGrey,
                                 ),
-                                primary: Colors.white,
-                                onPrimary: Colors.blueGrey,
                               ),
+                              primary: Colors.white,
+                              onPrimary: Colors.blueGrey,
                             ),
                           ),
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
+      ),
     );
   }
 }
